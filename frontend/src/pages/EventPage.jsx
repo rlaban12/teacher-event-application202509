ed25d35d04461fe2c209b81f19ef7e32a196d55b
soongu <naong526@naver.com>
2025-09-01 11:46:24 +0900

    [FE] 이벤트 상세조회 페이지 라우팅

--- frontend/src/pages/EventPage.jsx
+++ frontend/src/pages/EventPage.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const EventPage = () => {
 
@@ -22,7 +23,10 @@ const EventPage = () => {
     <>
       <h1>Event Page</h1>
       <ul>
-        {eventList.map(event => <li key={event.id}>{event.title}</li>)}
+        {eventList.map(({eventId, title}) =>
+          <li key={eventId}>
+            <Link to={`/events/${eventId}`}>{title}</Link>
+          </li>)}
       </ul>
     </>
   );
