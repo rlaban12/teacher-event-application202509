f126729d5e90d03236355642c3ae1200e7f3a9b5
soongu <naong526@naver.com>
2025-09-01 11:54:30 +0900

    [FE] 이벤트 상세조회 페이지에서 서버 데이터 요청하기

--- frontend/src/pages/EventDetailPage.jsx
+++ frontend/src/pages/EventDetailPage.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
 
 // /events/:id
 const EventDetailPage = () => {
+
+  const {eventId} = useParams();
+
+  const [detailEvent, setDetailEvent] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`http://localhost:9000/api/events/${eventId}`);
+      const data = await response.json();
+      console.log(data);
+      setDetailEvent(data);
+    })();
+  }, []);
+
   return (
     <>
       <h1>Event Detail Page</h1>
+      <p>Event Id: {eventId}</p>
+      <p>Event Title: {detailEvent?.title}</p>
+      <p>Event Description: {detailEvent?.desc}</p>
     </>
   );
 };
