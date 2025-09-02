1bb15ed3ed646782aea1e647c995d289512aea07
soongu <naong526@naver.com>
2025-09-02 11:06:27 +0900

    [BE] 무한스크롤 테스트

--- backend/src/test/java/com/study/event/repository/EventRepositoryImplTest.java
+++ backend/src/test/java/com/study/event/repository/EventRepositoryImplTest.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,19 +24,36 @@ class EventRepositoryImplTest {
     @Autowired
     EventRepository eventRepository;
 
-    @Test
-    @DisplayName("bulkInsert")
-    void bulkInsert() {
-        for (int i = 1; i <= 300; i++) {
-            Event event = Event.builder()
-                    .title("더미 제목 " + i)
-                    .description("더미 내용 " + i)
-                    .date(LocalDate.now())
-                    .image("https://kfescdn.visitkorea.or.kr/kfes/upload/contents/db/c34e66ec-0b3d-48be-96eb-c0ffbb6436ec_3.jpg")
-                    .build();
+//    @Test
+//    @DisplayName("bulkInsert")
+//    void bulkInsert() {
+//        for (int i = 1; i <= 300; i++) {
+//            Event event = Event.builder()
+//                    .title("더미 제목 " + i)
+//                    .description("더미 내용 " + i)
+//                    .date(LocalDate.now())
+//                    .image("https://kfescdn.visitkorea.or.kr/kfes/upload/contents/db/c34e66ec-0b3d-48be-96eb-c0ffbb6436ec_3.jpg")
+//                    .build();
+//
+//            eventRepository.save(event);
+//        }
+//    }
 
-            eventRepository.save(event);
-        }
+
+    @Test
+    @DisplayName("페이징 테스트")
+    void pagingTest() {
+        //given
+        int pageNo = 16;
+        int size = 20;
+        Pageable pageable = PageRequest.of(pageNo - 1, size);
+        //when
+        Slice<Event> eventSlice = eventRepository.findEvents(pageable);
+        List<Event> content = eventSlice.getContent();
+
+        //then
+        System.out.println("hasNext: " + eventSlice.hasNext());
+        System.out.println("size: " + content.size());
     }
 
 
