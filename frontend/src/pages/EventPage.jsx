0a1f7c1d48b5eed0635071ad040b17322ff1c24e
soongu <naong526@naver.com>
2025-09-02 12:11:52 +0900

    [FE] 스켈레톤 폴백

--- frontend/src/pages/EventPage.jsx
+++ frontend/src/pages/EventPage.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useRef, useState} from 'react';
 import EventList from '../components/EventList.jsx';
+import EventSkeleton from '../components/EventSkeleton.jsx';
 
 const EventPage = () => {
 
@@ -14,10 +15,18 @@ const EventPage = () => {
   // 더 이상 가져올 데이터가 있는지 여부
   const [isFinish, setIsFinish] = useState(false);
 
+  // 로딩 상태 관리
+  const [loading, setLoading] = useState(false);
+
 
   const fetchEvents = async () => {
 
-    if (isFinish) return;
+    if (isFinish || loading) return;
+
+    setLoading(true);
+
+    // 강제로 1.5초의 로딩 부여
+    await new Promise(r => setTimeout(r, 1500));
 
     const response = await fetch(`http://localhost:9000/api/events?page=${currentPage}`);
     const {hasNext, eventList: events} = await response.json();
@@ -25,6 +34,8 @@ const EventPage = () => {
     // 페이지번호 갱신
     setCurrentPage(prev => prev + 1);
     setIsFinish(!hasNext);
+
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -32,7 +43,7 @@ const EventPage = () => {
     // 무한스크롤을 위한 옵저버 생성
     const observer = new IntersectionObserver((entries) => {
 
-      if (isFinish) return;
+      if (isFinish || loading) return;
 
       if (entries[0].isIntersecting) {
         // console.log('감시대상 발견!');
@@ -56,8 +67,9 @@ const EventPage = () => {
     <>
       <EventList eventList={eventList} />
       {/* 무한스크롤 옵저버를 위한 감시대상 태그  */}
-      <div ref={observerRef} style={{ height: 300, background: 'yellow' }}>
+      <div ref={observerRef} style={{ height: 100 }}>
         {/* 로딩바, 스켈레톤 폴백 배치 */}
+        {loading && <EventSkeleton/>}
       </div>
     </>
   );
