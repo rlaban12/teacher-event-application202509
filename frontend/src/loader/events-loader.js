d75c9817cbecc790c4423cbcff84ea0f4d2052ef
soongu <naong526@naver.com>
2025-09-03 10:25:53 +0900

    [FE] API 연결정보 설정파일 생성

--- frontend/src/loader/events-loader.js
+++ frontend/src/loader/events-loader.js
@@ -1,6 +1,8 @@
 
+import { EVENT_API_URL } from '../config/host-config.js';
+
 export const eventListLoader = async () => {
-  const response = await fetch('http://localhost:9000/api/events?page=76');
+  const response = await fetch(`${EVENT_API_URL}?page=1`);
 
   // loader가 리턴한 데이터는 라우팅된 페이지와 그 하위 컴포넌트에서 언제든 사용가능
   // loader는 fetch결과를 바로 리턴하는 경우 알아서 json을 추출한다.
@@ -10,7 +12,7 @@ export const eventListLoader = async () => {
 
 export const eventDetailLoader = async ({ params }) => {
 
-  return await fetch(`http://localhost:9000/api/events/${params.eventId}`);
+  return await fetch(`${EVENT_API_URL}/${params.eventId}`);
 }
 
 
