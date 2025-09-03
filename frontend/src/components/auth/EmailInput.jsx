a2800b9e6ed5f5d04c6ce302f545b42017cdeeb8
soongu <naong526@naver.com>
2025-09-03 11:09:38 +0900

    [FE] 이메일 중복 검증

--- frontend/src/components/auth/EmailInput.jsx
+++ frontend/src/components/auth/EmailInput.jsx
@@ -1,5 +1,6 @@
 import {useEffect, useRef, useState} from 'react';
 import styles from './SignUpForm.module.scss';
+import {AUTH_API_URL} from '../../config/host-config.js';
 
 const EmailInput = () => {
 
@@ -23,6 +24,16 @@ const EmailInput = () => {
       setError('이메일이 올바르지 않습니다.');
       return;
     }
+
+    // 이메일 중복확인 검증
+    (async () => {
+      const response = await fetch(`${AUTH_API_URL}/check-email?email=${inputValue}`);
+      const { isDuplicate, message } = await response.json();
+      if (isDuplicate) {
+        setError(message);
+      }
+    })();
+
     setError('');
   };
 
