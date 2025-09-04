d1b5f24280ef56b1243221e30fcfb6f3b46f9cc1
soongu <naong526@naver.com>
2025-09-04 09:18:20 +0900

    [FE] 패스워드 검증 및 회원가입 버튼 활성화 처리

--- frontend/src/components/auth/SignUpForm.jsx
+++ frontend/src/components/auth/SignUpForm.jsx
@@ -8,8 +8,12 @@ import PasswordInput from './PasswordInput.jsx';
 const SignUpForm = () => {
 
   const [enteredEmail, setEnteredEmail] = useState('');
+
+  // 회원가입 버튼 활성화 여부
+  const [isActiveButton, setIsActiveButton] = useState(false);
+
   // 현재 어떤 스텝인지 확인
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(3);
   // 프로그레스바 노출 여부
   const [isNext, setIsNext] = useState(false);
 
@@ -29,12 +33,21 @@ const SignUpForm = () => {
     nextStep();
   };
 
+  // 패스워드 입력이 끝날 때 호출될 함수
+  const passwordSuccessHandler = (isValid) => {
+
+    // 회원가입버튼을 열어줄지 여부
+    setIsActiveButton(isValid);
+  };
+
   return (
     <div className={styles.signupForm}>
       <div className={styles.formStepActive}>
         {step === 1 && <EmailInput onSuccess={emailSuccessHandler} />}
         {step === 2 && <VerificationInput email={enteredEmail} onSuccess={nextStep} />}
-        {step === 3 && <PasswordInput />}
+        {step === 3 && <PasswordInput onSuccess={passwordSuccessHandler} />}
+
+        {isActiveButton && <div><button>회원가입 완료</button></div>}
 
         {isNext && <ProgressBar/>}
 
